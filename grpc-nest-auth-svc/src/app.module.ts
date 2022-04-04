dd1b9cc1696869e7bba150b623cc7191bcc51fff
gson Liang <yuanyu90221@gmail.com>
2022-04-04 17:11:42 +0800

    grpc-nest-auth-svc: add @grpc/grpc-js

--- grpc-nest-auth-svc/src/app.module.ts
+++ grpc-nest-auth-svc/src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { configValidationSchema } from './config.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env`],
+      validationSchema: configValidationSchema,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: config.get('TYPEORM_CONNECTION') as 'postgres',
+          host: config.get<string>('TYPEORM_HOST'),
+          username: config.get<string>('TYPEORM_USERNAME'),
+          password: config.get<string>('TYPEORM_PASSWORD'),
+          database: config.get<string>('TYPEORM_DATABASE'),
+          entities: ['dist/**/*.entity.{ts,js}'],
+          port: config.get<number>('TYPEORM_PORT'),
+          synchronize: true,
+        };
+      },
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ grpc-nest-auth-svc/src/auth/service/jwt.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService as Jwt } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { Auth } from '../auth.entity';
+
+@Injectable()
+export class JwtService {
+  @InjectRepository(Auth)
+  private readonly repository: Repository<Auth>;
+
+  private readonly jwt: Jwt;
+  constructor(jwt: Jwt) {
+    this.jwt = jwt;
+  }
+
+  public async decode(token: string): Promise<unknown> {
+    return this.jwt.decode(token, null);
+  }
+
+  public async validateUser(decoded: any): Promise<Auth> {
+    return this.repository.findOne(decoded.id);
+  }
+
+  public generateToken(auth: Auth): string {
+    return this.jwt.sign({ id: auth.id, email: auth.email });
+  }
+
+  public isPasswordValid(password: string, userPassword: string): boolean {
+    return bcrypt.compareSync(password, userPassword);
+  }
+
+  public encodePassword(passowrd: string): string {
+    const salt: string = bcrypt.genSaltSync(10);
+    return bcrypt.hashSync(passowrd, salt);
+  }
+
+  public async verify(token: string): Promise<any> {
+    try {
+      return this.jwt.verify(token);
+    } catch (error) {
+      throw error;
+    }
+  }
+}
